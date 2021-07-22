3b88437012bbc00c89dd46f4831227871acf5af4
NatalieKolcheva <natalykolcheva@gmail.com>
2021-07-22 09:16:19 +0300

    added functionality for calculation the sum of odd and even numbers from the interval

--- src/main/java/org/lessons/Main.java
+++ src/main/java/org/lessons/Main.java
@@ -1,7 +1,30 @@
 package org.lessons;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        //variables
+        int sumOdd = 0;
+        int sumEven = 0;
+        int i;
+        //input
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input beginning of the interval:");
+        int inputBegin = input.nextInt();
+        System.out.println("Input ending of the interval:");
+        int inputEnd = input.nextInt();
+        //calculation
+        i = inputBegin;
+        while (i <= inputEnd) {
+            if (i % 2 == 0) {
+                sumEven = sumEven + i;
+            } else
+                sumOdd = sumOdd + i;
+            i++;
+        }
+        //out
+        System.out.println("Result Even numbers:"+sumEven);
+        System.out.println("Result Odd numbers:"+sumOdd);
     }
 }
